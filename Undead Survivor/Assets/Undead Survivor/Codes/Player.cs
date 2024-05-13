bc0cda8ea225ab6b1205c59bc4d2af904ff963bb
eodevelop <eodevelop@naver.com>
2024-05-13 17:19:28 +0900

    1.애니메이션 추가

--- Undead Survivor/Assets/Undead Survivor/Codes/Player.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/Player.cs
@@ -9,11 +9,13 @@ public class Player : MonoBehaviour
 
     Rigidbody2D rigid;
     SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -30,6 +32,8 @@ public class Player : MonoBehaviour
 
     private void LateUpdate()
     {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
         if (inputVec.x != 0) {
             spriter.flipX = inputVec.x < 0;
         }
