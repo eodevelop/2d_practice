8232f89b446abd41f3a6d2664e2a6156568ec1bd
eodevelop <eodevelop@naver.com>
2024-05-16 22:45:52 +0900

    맵 이동 제대로 안됌 임시 커밋

--- Undead Survivor/Assets/Undead Survivor/Codes/GameManager.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/GameManager.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
+    public Player player;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        instance = this;
     }
 }
--- Undead Survivor/Assets/Undead Survivor/Codes/Reposition.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/Reposition.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerExit2D(Collider2D collision)
     {
-        
-    }
+        if (!collision.CompareTag("Area")) 
+        {
+            return;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
         
+        float diffx = Mathf.Abs(dirX);
+        float diffy = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch (transform.tag) {
+            case "Ground":
+                if (diffx > diffy) {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if(diffx < diffy) {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+                
+                break;
+        }
     }
 }
